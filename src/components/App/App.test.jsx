b156cd62d1ed1e8d4b983213c7cace535aa0796d
Michael-Plat <67632735+Michael-Plat@users.noreply.github.com>
2021-07-16 19:04:21 +0300

    integration tests react

--- src/components/App/App.test.jsx
+++ src/components/App/App.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
 describe('App component', () => {
@@ -6,5 +7,31 @@ describe('App component', () => {
     render(<App />)
 
     expect(screen.getByRole('list')).toBeInTheDocument();
+    expect(screen.getByText('Find course:')).toBeInTheDocument();
+  });
+  it('typing Searchbox works', () => {
+    render(<App />);
+
+    expect(screen.queryByDisplayValue(/React/i)).toBeNull();
+
+    userEvent.type(screen.getByRole('textbox'), 'React');
+
+    expect(screen.queryByDisplayValue(/React/)).toBeInTheDocument();
+  });
+  it('search filter is  working', () => {
+    render(<App />);
+
+    expect(screen.getByText(/Vue/)).toBeInTheDocument();
+    expect(screen.getByText(/JavaScript/)).toBeInTheDocument();
+
+    userEvent.type(screen.getByRole('textbox'), 'script');
+
+    expect(screen.queryByText(/Vue/)).toBeNull();
+    expect(screen.queryByText(/JavaScript/)).toBeInTheDocument();
+  });
+  it('App snapshot', () => {
+    const app = render(<App />);
+
+    expect(app).toMatchSnapshot();
   });
 });
